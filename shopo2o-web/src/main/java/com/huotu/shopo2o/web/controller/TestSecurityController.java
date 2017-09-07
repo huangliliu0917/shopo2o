cead3f805dc7ce125d4c3e12c80487b46c96254f
huxuanhao2017 <hu_xuanhao@163.com>
2017-09-07 11:02:46 +0800

    security配置

--- shopo2o-web/src/main/java/com/huotu/shopo2o/web/controller/TestSecurityController.java
+++ shopo2o-web/src/main/java/com/huotu/shopo2o/web/controller/TestSecurityController.java
@@ -1,13 +1,10 @@
 package com.huotu.shopo2o.web.controller;
 
 import com.huotu.shopo2o.service.entity.MallCustomer;
-import com.huotu.shopo2o.service.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by hxh on 2017-08-31.
@@ -15,8 +12,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class TestSecurityController {
 
-    @Autowired
-    private LoginService loginService;
 
     @RequestMapping(value = {"", "/", "/login"})
     public String index() {
@@ -24,7 +19,7 @@ public class TestSecurityController {
     }
 
     @RequestMapping("/loginFailed")
-    public String loginFailed(RedirectAttributes attributes) {
+    public String loginFailed() {
         return "redirect:login";
     }
 
