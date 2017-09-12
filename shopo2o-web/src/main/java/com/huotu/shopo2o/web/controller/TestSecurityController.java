0080188e39bca38345bee99bec923f56caec907a
huxuanhao2017 <hu_xuanhao@163.com>
2017-09-12 13:55:42 +0800

    页面修改

--- shopo2o-web/src/main/java/com/huotu/shopo2o/web/controller/TestSecurityController.java
+++ shopo2o-web/src/main/java/com/huotu/shopo2o/web/controller/TestSecurityController.java
@@ -23,9 +23,23 @@ public class TestSecurityController {
         return "redirect:login";
     }
 
+//    @RequestMapping("/loginSuccess")
+//    public String loginSuccess(@AuthenticationPrincipal MallCustomer mallCustomer, Model model) {
+//        model.addAttribute("mallCustomer", mallCustomer);
+//        return "homeV2";
+//    }
+    /**
+     * 登录验证通过后更新登录时间并跳转供应商后台首页
+     *
+     * @param mallCustomer
+     * @return
+     */
     @RequestMapping("/loginSuccess")
-    public String loginSuccess(@AuthenticationPrincipal MallCustomer mallCustomer, Model model) {
-        model.addAttribute("mallCustomer", mallCustomer);
-        return "home";
+    public String loginSuccess(
+            @AuthenticationPrincipal MallCustomer mallCustomer,
+            Model model
+    ) {
+        return "homeV2";
     }
+
 }
